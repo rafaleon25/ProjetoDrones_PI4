c6970181d305b6863cad5c1b8b0b65e6048f44c6
rafaleon25 <rafa.leon25@gmail.com>
2018-05-12 18:18:57 -0300

    Atualização ControlerVenda

--- src/main/java/com/drone/ProjetoDrone/Controllers/VendaController.java
+++ src/main/java/com/drone/ProjetoDrone/Controllers/VendaController.java
@@ -5,8 +5,20 @@
  */
 package com.drone.ProjetoDrone.Controllers;
 
+import com.drone.ProjetoDrone.Classes.Cliente.Cliente;
+import com.drone.ProjetoDrone.Classes.Produto.Produto;
+import com.drone.ProjetoDrone.Classes.Venda.Venda;
+import com.drone.ProjetoDrone.Classes.Venda.VendaProd;
+import com.drone.ProjetoDrone.Repository.VendaRepository;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,8 +29,41 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/venda")
 public class VendaController {
+    
+    @Autowired
+    private VendaRepository repository;
+    
     @GetMapping
-    public ModelAndView telaVenda(){
-        return new ModelAndView("FinalizarCompra");
+    public ModelAndView telaVenda(HttpSession sessao){
+        Venda venda = new Venda();
+        Cliente cli = (Cliente) sessao.getAttribute("usuario");
+        venda.setCliente(cli);
+        
+        List<Produto> carrinho = (List<Produto>) sessao.getAttribute("carrinho");
+        
+        Set<VendaProd> vendaProd = new LinkedHashSet<>();
+        for (int i = 0; i < carrinho.size(); i++) {
+            VendaProd vendaprodCar = new VendaProd();
+            vendaprodCar.setProduto(carrinho.get(i));
+            vendaprodCar.setQtd(carrinho.get(i).getQuantidadeUsu());
+            vendaProd.add(vendaprodCar);
+        }
+        venda.setVendaProd(vendaProd);
+        
+        double total = 0;
+        
+        for (int i = 0; i < carrinho.size(); i++) {
+            total = total + (carrinho.get(i).getPrecos().getPreco() * carrinho.get(i).getQuantidadeUsu());
+        }
+        venda.setTotalVenda(total);
+        return new ModelAndView("FinalizarCompra").addObject("venda", venda);
+    }
+    
+    @PostMapping("/finalizar")
+    public ModelAndView finalizar(HttpSession sessao, @ModelAttribute("venda") Venda venda){
+        
+        repository.incluir(venda);
+        
+        return new ModelAndView("Resumo");
     }
 }
